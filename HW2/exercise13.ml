75808550e8a145df9bd61a507f92d7219b73ada0
EunhoKang <zay0njugin@gmail.com>
2023-04-03 21:35:22 +0900

    Update exercise13.ml

---
+++ HW2/exercise13.ml
@@ -0,0 +1,16 @@
+let rec nth l n =
+	match l with
+	| [] -> raise (Failure "list is too short")
+	| hd::tl -> if n = 0 then hd else nth tl (n - 1)
+
+let rec makeSubset l ol n =
+  match n with 
+  | -1 -> [l]
+  | _ -> (makeSubset l ol (n - 1))@(makeSubset ((nth ol n)::l) ol (n - 1)) 
+
+let rec length l =
+  match l with
+  | [] -> 0
+  | hd::tl -> 1 + length tl
+
+let rec powerset l = makeSubset [] l ((length l)-1);;
